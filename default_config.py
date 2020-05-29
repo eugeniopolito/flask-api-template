a99eb780f022ea6eb613ecf4a3d131c33357d4f8
Eugenio Polito <e.polito@peachwire.com>
2020-05-29 16:12:35 +0200

    a bit of refactoring

--- default_config.py
+++ default_config.py
@@ -3,12 +3,12 @@ import os
 DEBUG = True
 
 # Docker MariaDB
-SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 PROPAGATE_EXCEPTIONS = True
 
-JWT_SECRET_KEY = os.environ["JWT_SECRET_KEY"]
-SECRET_KEY = os.environ["JWT_SECRET_KEY"]
+JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "12345")
+SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "12345")
 JWT_BLACKLIST_ENABLED = True
 JWT_BLACKLIST_TOKEN_CHECKS = [
     "access",
